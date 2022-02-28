bd52f3acb9366767797294ff7417e3c12abd6861
Snehil Seenu <80764244+Mijokotsi@users.noreply.github.com>
2022-02-28 20:53:12 +0530

    Update 242-valid-anagram.cpp

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        
+        // Srting both the strings
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         
+        // Checking id both the strings are equal or not
         if(s.compare(t) == 0)
             return true;
         else
