9a0c784b82996d44abae3417a9e4394c061ab32c
Snehil Seenu <80764244+Mijokotsi@users.noreply.github.com>
2022-03-02 18:17:17 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -2,21 +2,33 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         
-        string ans = "";
+        // To hold the string which we extract out after comparing s with t
+        string ans = ""; 
+        
+        // variable to hold the index of the previous element checked
+        // so that we can start to check elements ahead of it from next iteration
         int prev = -1;
+        
+        // vector to check is the element already visited or not
+        // false = not visited
+        // true = visited
         vector<bool> vis (s.length(), false);
         
+        // Loop for checking with TC: O(n^2)
         for(int i=0;i<s.length();i++) {
             for(int j=prev+1;j<t.length();j++) {
+                
+                // If the elements are equal and not visited yet
                 if(s[i] == t[j] and vis[i] == false) {
-                    ans += s[i];
-                    prev = j;
-                    vis[i] = true;
+                    ans += s[i];   // append it to ans string
+                    prev = j;      // Setting new j with this element's index
+                    vis[i] = true;  // marking it as visited
                 }
                     
             }
         }
         
+        // comapring the extracted string with the subsequence
         return (s.compare(ans) == 0);
     }
 };
