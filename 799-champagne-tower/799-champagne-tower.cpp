099f87977c351e1e98bed736e1a0d93e6590e894
Snehil Seenu <80764244+Mijokotsi@users.noreply.github.com>
2022-03-04 21:47:33 +0530

    Time: 20 ms (44.56%), Space: 43.1 MB (9.62%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -9,18 +9,24 @@ public:
         // dp[0][0] = poured
         dp[0][0] = poured;
         
-        // Running the loop
+        // Running the loop for traversing all the glasses
         for(int i = 0; i < 100; i++)
             for(int j = 0; j <= i; j++)
                 
+                // If there's an overflow
                 if(dp[i][j] > 1) {
                     
+                    // calculate the remaining and add it equally to 
+                    // its children (left and right)
                     double remaining = (dp[i][j] - 1);
                     dp[i + 1][j] += remaining/2;
                     dp[i + 1][j + 1] += remaining / 2;
+                    
+                    // And set its value to 1 as its fully filled now
                     dp[i][j] = 1;
                 }
-    
+
+        // Finally return the amaount filled in the glass you require
         return dp[query_row][query_glass];
     }
 };
