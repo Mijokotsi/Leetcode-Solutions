812d1faea928780f61dd890512c4d8c4680a0561
Snehil Seenu <80764244+seenusnehil@users.noreply.github.com>
2022-07-30 18:09:54 +0530

    Time: 4 ms (55.37%), Space: 5.9 MB (73.77%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -4,8 +4,9 @@ public:
         
         int s = 0;
         int e = x/2;
-        long long int ans = -1;
+        long long int ans = -1;    // To hold the value of the square root
         
+        // Base case
         if(x == 1 || x == 0)
             return x;
         
@@ -14,10 +15,15 @@ public:
             long long int mid = s + (e-s)/2;
             long long int sq = mid*mid;
             
+            // So, if the square is less than the number given, then we need to move ahead
+            // i.e. to a bigger value
+            // else if it is greater, then we need to move towards lower value
             if(sq == x)
                 return mid;
             else if(sq > x)
                 e = mid-1;
+            
+            // Before moving ahead, store the previous value in ans variable
             else {
                 ans = mid;
                 s = mid+1;
