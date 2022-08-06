e5459a8963bdacd192f3ed446e46ecceec37f9aa
Snehil Seenu <80764244+seenusnehil@users.noreply.github.com>
2022-08-06 21:26:51 +0530

    Time: 10 ms (32.37%), Space: 9.8 MB (22.59%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         
+        // declaring the variables
         int s = 0, e = nums.size()-1;
         int ans = 0;
+        
+        // edge case: if the target is greater than the last element
+        // as the array is sorted, so there just return position next to last index
         
         if(target > nums[e])
             return e+1;
         
+        // looping and following the same procedure of binary search
         while(s <= e) {
             int mid = s + (e-s)/2;
             
             if(target > nums[mid]) {
+                // incrementing s and at the same time, storing the index
                 s = mid+1;
                 ans = s;
             }
@@ -22,6 +28,7 @@ public:
                 return mid;
         }
         
+        // returning the answer
         return ans;
     }
 };
