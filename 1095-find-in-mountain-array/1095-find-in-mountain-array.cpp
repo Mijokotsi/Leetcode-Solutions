c3d757bd5cd35fc7c2c53a16533cb98e17e9f361
Snehil Seenu <80764244+seenusnehil@users.noreply.github.com>
2022-08-14 17:32:18 +0530

    Time: 5 ms (69.56%), Space: 6.9 MB (88.21%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -11,39 +11,48 @@
 class Solution {
 public:
     
+    // So the approach we are going to follow is as follows:
+    // 1. Find the peak element in the array
+    // 2. Search for the target element in the ascending part of the array, if not found
+    // search in the descending part, and return the index where 
+    
+    // Function to perform orderAgnostic binary search
     int binary(MountainArray &mountainArr, int s, int e, int target) {
         
         // Check if the array is sorted in asceding or descending order
-    bool isAsc = mountainArr.get(s) < mountainArr.get(e);
+        bool isAsc = mountainArr.get(s) < mountainArr.get(e);
 
-    while(s <= e) {
-        // In this way the mid will never exceed the value of int in cpp
-        int mid = s + (e-s)/2;
+        while(s <= e) {
+            // In this way the mid will never exceed the value of int in cpp
+            int mid = s + (e-s)/2;
 
-        if(target == mountainArr.get(mid)) 
-            return mid;
-
-        if(isAsc) {
-            if(target < mountainArr.get(mid)) {
-                e = mid-1;
+            if(target == mountainArr.get(mid)) 
+                return mid;
+            
+            // if the array is sorted in ascending order
+            if(isAsc) {
+                if(target < mountainArr.get(mid)) {
+                    e = mid-1;
+                }
+                else {
+                    s = mid+1;
+                } 
             }
+            // Else if the array is sorted in descending order
             else {
-                s = mid+1;
-            } 
-        }
-        else {
-            if(target > mountainArr.get(mid)) {
-                e = mid-1;
+                if(target > mountainArr.get(mid)) {
+                    e = mid-1;
+                }
+                else {
+                    s = mid+1;
+                } 
             }
-            else {
-                s = mid+1;
-            } 
         }
-    }
 
-    return -1;
+        return -1;
     }
     
+    // Function to find the peak element in the mountain array
     int peakElement(MountainArray &mountainArr) {
         
         int start = 0;
@@ -73,13 +82,17 @@ public:
         return start;
     }
     
+    // Function to find the first position of the target element in the given array
     int findInMountainArray(int target, MountainArray &mountainArr) {
+        // stores the index of the peak element
         int peak = peakElement(mountainArr);
         
+        // Searching in the left array
         int left = binary(mountainArr, 0, peak, target);
         if(left != -1)
             return left;
         else
+            // searching in the right array
             return binary(mountainArr, peak+1, mountainArr.length() -1, target);
        
     }
