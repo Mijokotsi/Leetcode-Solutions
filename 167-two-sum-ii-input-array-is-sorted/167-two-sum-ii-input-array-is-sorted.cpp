188c52c85b52286a717f538b87a09d906dc6f5b8
Snehil Seenu <80764244+seenusnehil@users.noreply.github.com>
2022-08-16 15:31:28 +0530

    Time: 22 ms (50.26%), Space: 15.7 MB (44.30%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -5,21 +5,22 @@ public:
         
         int start = 0;
         int end = ar.size()-1;
+        int sum = 0;
         vector<int> ans;     // vector to store the array
         
         // Loop will end as soon as we get our pair
         while(start <= end) {
-            
+            sum = ar[start] + ar[end];
             // Check if the sum of start and emd element is greater than the target
             // then move end pointer back, as now we need to reduce the sum in order to match
             // with the target
-            if(ar[start] + ar[end] > target)
+            if(sum > target)
                 end--;
             
             // Check if the sum of start and emd element is smaller than the target
             // then move start pointer ahead, as now we need to increase the sum in order to
             // match with the target
-            else if(ar[start] + ar[end] < target)
+            else if(sum < target)
                 start++;
             
             // Else we've found the match, hence push the index to the answer array in
