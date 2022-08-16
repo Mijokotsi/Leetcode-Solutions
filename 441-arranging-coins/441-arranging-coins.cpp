dbaf035616dd0eaae639cda4c07c20f75d0e9860
Snehil Seenu <80764244+seenusnehil@users.noreply.github.com>
2022-08-16 16:41:44 +0530

    Time: 4 ms (71.61%), Space: 5.9 MB (25.11%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        
+        int count = 0;
+        int i=1;
+        while(n >= i) {
+            n = n-i;
+            count++;
+            
+            i++;
+        }
+        
+        return count;
+    }
+};
